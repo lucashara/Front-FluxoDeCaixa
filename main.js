8fc5de766466b2b0ff8bdf31fdc86e2b28d640b1
Lucas Harã <lucashara20@gmail.com>
2024-01-24 16:30:17 -0300

    grafico de fluxo de caixa

--- main.js
+++ main.js
@@ -64,6 +64,19 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
+  
+  
+  async function buscarDadosFluxoDeCaixaDia() {
+    const apiUrl = `https://192.168.121.145:4445/fluxo_de_caixa_dia?data_inicial=${formatarData(dataInicialInput.value)}&data_final=${formatarData(dataFinalInput.value)}`;
+    try {
+        const dados = await buscarDadosAPI(apiUrl);
+        criarGraficoFluxoDeCaixa(dados);
+    } catch (error) {
+        console.error("Erro ao buscar dados para o gráfico:", error);
+    }
+  }
+
+
   function definirCorDoValor(valorFormatado, elemento) {
     // Removendo o símbolo de moeda e convertendo para número
     const valor = Number(valorFormatado.replace(/R\$\s?|(\.)/g, '').replace(',', '.'));
@@ -88,67 +101,66 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   // Processa e exibe os KPIs de Contas a Receber/Pagar
-  // Processa e exibe os KPIs de Contas a Receber/Pagar
-function processarKPIsContas(dados) {
-  let totalReceberPagoGrupo = 0;
-  let totalReceberTituloGrupo = 0;
-  let totalPagarPagoGrupo = 0;
-  let totalPagarTituloGrupo = 0;
-  let totalSaldoDisponivelGrupo = 0;
-  let totalSaldoPrevistoGrupo = 0;
+  function processarKPIsContas(dados) {
+    let totalReceberPagoGrupo = 0;
+    let totalReceberTituloGrupo = 0;
+    let totalPagarPagoGrupo = 0;
+    let totalPagarTituloGrupo = 0;
+    let totalSaldoDisponivelGrupo = 0;
+    let totalSaldoPrevistoGrupo = 0;
 
-  dados.forEach(dado => {
-      const { CODFILIAL, RECEBER_VALORPAGO, RECEBER_VALORTITULO, PAGAR_VALORPAGO, PAGAR_VALORTITULO, SALDO_DISP, SALDO_PREVISTO } = dado;
+    dados.forEach(dado => {
+        const { CODFILIAL, RECEBER_VALORPAGO, RECEBER_VALORTITULO, PAGAR_VALORPAGO, PAGAR_VALORTITULO, SALDO_DISP, SALDO_PREVISTO } = dado;
 
-      // Soma os totais para o Grupo BRF1
-      totalReceberPagoGrupo += RECEBER_VALORPAGO;
-      totalReceberTituloGrupo += RECEBER_VALORTITULO;
-      totalPagarPagoGrupo += PAGAR_VALORPAGO;
-      totalPagarTituloGrupo += PAGAR_VALORTITULO;
-      totalSaldoDisponivelGrupo += SALDO_DISP;
-      totalSaldoPrevistoGrupo += SALDO_PREVISTO;
+        // Soma os totais para o Grupo BRF1
+        totalReceberPagoGrupo += RECEBER_VALORPAGO;
+        totalReceberTituloGrupo += RECEBER_VALORTITULO;
+        totalPagarPagoGrupo += PAGAR_VALORPAGO;
+        totalPagarTituloGrupo += PAGAR_VALORTITULO;
+        totalSaldoDisponivelGrupo += SALDO_DISP;
+        totalSaldoPrevistoGrupo += SALDO_PREVISTO;
 
-      // Atualiza os valores e cores para cada filial
-      if (kpis.hasOwnProperty(CODFILIAL)) {
-          kpis[CODFILIAL].receberPago.textContent = formatarMoeda(RECEBER_VALORPAGO);
-          definirCorDoValor(kpis[CODFILIAL].receberPago.textContent, kpis[CODFILIAL].receberPago);
+        // Atualiza os valores e cores para cada filial
+        if (kpis.hasOwnProperty(CODFILIAL)) {
+            kpis[CODFILIAL].receberPago.textContent = formatarMoeda(RECEBER_VALORPAGO);
+            definirCorDoValor(kpis[CODFILIAL].receberPago.textContent, kpis[CODFILIAL].receberPago);
 
-          kpis[CODFILIAL].receberTitulo.textContent = formatarMoeda(RECEBER_VALORTITULO);
-          definirCorDoValor(kpis[CODFILIAL].receberTitulo.textContent, kpis[CODFILIAL].receberTitulo);
+            kpis[CODFILIAL].receberTitulo.textContent = formatarMoeda(RECEBER_VALORTITULO);
+            definirCorDoValor(kpis[CODFILIAL].receberTitulo.textContent, kpis[CODFILIAL].receberTitulo);
 
-          kpis[CODFILIAL].pagarPago.textContent = formatarMoeda(PAGAR_VALORPAGO);
-          definirCorDoValor(kpis[CODFILIAL].pagarPago.textContent, kpis[CODFILIAL].pagarPago);
+            kpis[CODFILIAL].pagarPago.textContent = formatarMoeda(PAGAR_VALORPAGO);
+            definirCorDoValor(kpis[CODFILIAL].pagarPago.textContent, kpis[CODFILIAL].pagarPago);
 
-          kpis[CODFILIAL].pagarTitulo.textContent = formatarMoeda(PAGAR_VALORTITULO);
-          definirCorDoValor(kpis[CODFILIAL].pagarTitulo.textContent, kpis[CODFILIAL].pagarTitulo);
+            kpis[CODFILIAL].pagarTitulo.textContent = formatarMoeda(PAGAR_VALORTITULO);
+            definirCorDoValor(kpis[CODFILIAL].pagarTitulo.textContent, kpis[CODFILIAL].pagarTitulo);
 
-          kpis[CODFILIAL].saldoDisponivel.textContent = formatarMoeda(SALDO_DISP);
-          definirCorDoValor(kpis[CODFILIAL].saldoDisponivel.textContent, kpis[CODFILIAL].saldoDisponivel);
+            kpis[CODFILIAL].saldoDisponivel.textContent = formatarMoeda(SALDO_DISP);
+            definirCorDoValor(kpis[CODFILIAL].saldoDisponivel.textContent, kpis[CODFILIAL].saldoDisponivel);
 
-          kpis[CODFILIAL].saldoPrevisto.textContent = formatarMoeda(SALDO_PREVISTO);
-          definirCorDoValor(kpis[CODFILIAL].saldoPrevisto.textContent, kpis[CODFILIAL].saldoPrevisto);
-      }
-  });
+            kpis[CODFILIAL].saldoPrevisto.textContent = formatarMoeda(SALDO_PREVISTO);
+            definirCorDoValor(kpis[CODFILIAL].saldoPrevisto.textContent, kpis[CODFILIAL].saldoPrevisto);
+        }
+    });
 
-  // Atualiza e define as cores dos totais para o Grupo BRF1
-  kpis.grupoBRF1.receberPago.textContent = formatarMoeda(totalReceberPagoGrupo);
-  definirCorDoValor(kpis.grupoBRF1.receberPago.textContent, kpis.grupoBRF1.receberPago);
+    // Atualiza e define as cores dos totais para o Grupo BRF1
+    kpis.grupoBRF1.receberPago.textContent = formatarMoeda(totalReceberPagoGrupo);
+    definirCorDoValor(kpis.grupoBRF1.receberPago.textContent, kpis.grupoBRF1.receberPago);
 
-  kpis.grupoBRF1.receberTitulo.textContent = formatarMoeda(totalReceberTituloGrupo);
-  definirCorDoValor(kpis.grupoBRF1.receberTitulo.textContent, kpis.grupoBRF1.receberTitulo);
+    kpis.grupoBRF1.receberTitulo.textContent = formatarMoeda(totalReceberTituloGrupo);
+    definirCorDoValor(kpis.grupoBRF1.receberTitulo.textContent, kpis.grupoBRF1.receberTitulo);
 
-  kpis.grupoBRF1.pagarPago.textContent = formatarMoeda(totalPagarPagoGrupo);
-  definirCorDoValor(kpis.grupoBRF1.pagarPago.textContent, kpis.grupoBRF1.pagarPago);
+    kpis.grupoBRF1.pagarPago.textContent = formatarMoeda(totalPagarPagoGrupo);
+    definirCorDoValor(kpis.grupoBRF1.pagarPago.textContent, kpis.grupoBRF1.pagarPago);
 
-  kpis.grupoBRF1.pagarTitulo.textContent = formatarMoeda(totalPagarTituloGrupo);
-  definirCorDoValor(kpis.grupoBRF1.pagarTitulo.textContent, kpis.grupoBRF1.pagarTitulo);
+    kpis.grupoBRF1.pagarTitulo.textContent = formatarMoeda(totalPagarTituloGrupo);
+    definirCorDoValor(kpis.grupoBRF1.pagarTitulo.textContent, kpis.grupoBRF1.pagarTitulo);
 
-  kpis.grupoBRF1.saldoDisponivel.textContent = formatarMoeda(totalSaldoDisponivelGrupo);
-  definirCorDoValor(kpis.grupoBRF1.saldoDisponivel.textContent, kpis.grupoBRF1.saldoDisponivel);
+    kpis.grupoBRF1.saldoDisponivel.textContent = formatarMoeda(totalSaldoDisponivelGrupo);
+    definirCorDoValor(kpis.grupoBRF1.saldoDisponivel.textContent, kpis.grupoBRF1.saldoDisponivel);
 
-  kpis.grupoBRF1.saldoPrevisto.textContent = formatarMoeda(totalSaldoPrevistoGrupo);
-  definirCorDoValor(kpis.grupoBRF1.saldoPrevisto.textContent, kpis.grupoBRF1.saldoPrevisto);
-}
+    kpis.grupoBRF1.saldoPrevisto.textContent = formatarMoeda(totalSaldoPrevistoGrupo);
+    definirCorDoValor(kpis.grupoBRF1.saldoPrevisto.textContent, kpis.grupoBRF1.saldoPrevisto);
+  }
 
 
 
@@ -204,7 +216,7 @@ function processarKPIsContas(dados) {
   // Inicia o temporizador para atualização automática
   function iniciarTemporizador() {
       pararTemporizador();
-      updateTimer = setInterval(atualizarTodosKPIs, 60000);
+      updateTimer = setInterval(atualizarTodosKPIs, 2000);
   }
 
   // Para o temporizador de atualização automática
@@ -227,36 +239,73 @@ function processarKPIsContas(dados) {
 
   // Atualiza todos os KPIs (Contas e Saldos)
   async function atualizarTodosKPIs() {
-      if (!validarDatas(dataInicialInput.value, dataFinalInput.value)) {
-          alert("Erro: A data inicial não pode ser maior que a data final.");
-          return;
-      }
-      await atualizarKPIsContas();
-      await atualizarSaldosBanco();
+    if (!validarDatas(dataInicialInput.value, dataFinalInput.value)) {
+        alert("Erro: A data inicial não pode ser maior que a data final.");
+        return;
+    }
+    await atualizarKPIsContas();
+    await atualizarSaldosBanco();
+    await buscarDadosFluxoDeCaixaDia(); // Atualiza o gráfico
   }
 
   pesquisarButton.addEventListener("click", atualizarTodosKPIs);
   buscarAcumuladoMesAtual();
 
-  // Busca o acumulado do mês atual ao carregar a página
-  async function buscarAcumuladoMesAtual() {
+  let graficoFluxoDeCaixa = null; // Variável global para armazenar o gráfico
+
+
+  function criarGraficoFluxoDeCaixa(dados) {
+    // Destruir o gráfico existente, se houver
+    if (graficoFluxoDeCaixa) {
+        graficoFluxoDeCaixa.destroy();
+    }
+
+    // Processar os dados para o formato necessário pelo Chart.js
+    const labels = dados.map(d => d.DTVENC);
+    const saldoDisponivelData = dados.map(d => d.SALDO_DISP);
+    const saldoPrevistoData = dados.map(d => d.SALDO_PREVISTO);
+
+    const ctx = document.getElementById('graficoFluxoDeCaixa').getContext('2d');
+    graficoFluxoDeCaixa = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Saldo Disponível',
+                data: saldoDisponivelData,
+                borderColor: 'rgb(54, 162, 235)',
+                tension: 0.1
+            }, {
+                label: 'Saldo Previsto',
+                data: saldoPrevistoData,
+                borderColor: 'rgb(255, 99, 132)',
+                tension: 0.1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+  }
+
+    // Busca o acumulado do mês atual até a data atual ao carregar a página
+    async function buscarAcumuladoMesAtual() {
       const dataAtual = new Date();
       const ano = dataAtual.getFullYear();
       const mes = (dataAtual.getMonth() + 1).toString().padStart(2, "0");
       const primeiroDiaDoMes = "01";
-      const ultimoDiaDoMes = new Date(ano, dataAtual.getMonth() + 1, 0).getDate();
-      const dataInicialFormatada = formatarData(
-        `${ano}-${mes}-${primeiroDiaDoMes}`
-      );
-      const dataFinalFormatada = formatarData(
-        `${ano}-${mes}-${ultimoDiaDoMes.toString().padStart(2, "0")}`
-      );
-  
+      const diaAtual = dataAtual.getDate().toString().padStart(2, "0"); // Dia atual
+
+      const dataInicialFormatada = formatarData(`${ano}-${mes}-${primeiroDiaDoMes}`);
+      const dataFinalFormatada = formatarData(`${ano}-${mes}-${diaAtual}`);
+
       dataInicialInput.value = `${ano}-${mes}-${primeiroDiaDoMes}`;
-      dataFinalInput.value = `${ano}-${mes}-${ultimoDiaDoMes
-        .toString()
-        .padStart(2, "0")}`;
-        atualizarTodosKPIs();
+      dataFinalInput.value = `${ano}-${mes}-${diaAtual}`; // Define a data final como hoje
+      await atualizarTodosKPIs();
       iniciarTemporizador();
     }
   });
