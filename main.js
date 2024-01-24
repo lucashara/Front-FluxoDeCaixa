ed9c385ce62853acc5ce29e07c276ba51067d5b6
Lucas Harã <lucashara20@gmail.com>
2024-01-24 16:58:00 -0300

    reorganizado js

--- main.js
+++ main.js
@@ -1,47 +1,47 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Elementos de interface
+  // Seletores de elementos da interface
   const dataInicialInput = document.getElementById("dataInicial");
   const dataFinalInput = document.getElementById("dataFinal");
   const pesquisarButton = document.getElementById("pesquisar");
-
   // Seletores para os KPIs de cada filial e do grupo
   const kpis = {
-      grupoBRF1: {
-        receberPago: document.getElementById("receberPagoGrupo"),
-        receberTitulo: document.getElementById("receberTituloGrupo"),
-        pagarPago: document.getElementById("pagarPagoGrupo"),
-        pagarTitulo: document.getElementById("pagarTituloGrupo"),
-        saldoDisponivel: document.getElementById("saldoDisponivelGrupo"),
-        saldoPrevisto: document.getElementById("saldoPrevistoGrupo"),
-      },
-      1: {
-        receberPago: document.getElementById("receberPagoFilial1"),
-        receberTitulo: document.getElementById("receberTituloFilial1"),
-        pagarPago: document.getElementById("pagarPagoFilial1"),
-        pagarTitulo: document.getElementById("pagarTituloFilial1"),
-        saldoDisponivel: document.getElementById("saldoDisponivelFilial1"),
-        saldoPrevisto: document.getElementById("saldoPrevistoFilial1"),
-      },
-      2: {
-        receberPago: document.getElementById("receberPagoFilial2"),
-        receberTitulo: document.getElementById("receberTituloFilial2"),
-        pagarPago: document.getElementById("pagarPagoFilial2"),
-        pagarTitulo: document.getElementById("pagarTituloFilial2"),
-        saldoDisponivel: document.getElementById("saldoDisponivelFilial2"),
-        saldoPrevisto: document.getElementById("saldoPrevistoFilial2"),
-      },
-      3: {
-        receberPago: document.getElementById("receberPagoFilial3"),
-        receberTitulo: document.getElementById("receberTituloFilial3"),
-        pagarPago: document.getElementById("pagarPagoFilial3"),
-        pagarTitulo: document.getElementById("pagarTituloFilial3"),
-        saldoDisponivel: document.getElementById("saldoDisponivelFilial3"),
-        saldoPrevisto: document.getElementById("saldoPrevistoFilial3"),
-      },
-    };
-  let updateTimer;
+    grupoBRF1: {
+      receberPago: document.getElementById("receberPagoGrupo"),
+      receberTitulo: document.getElementById("receberTituloGrupo"),
+      pagarPago: document.getElementById("pagarPagoGrupo"),
+      pagarTitulo: document.getElementById("pagarTituloGrupo"),
+      saldoDisponivel: document.getElementById("saldoDisponivelGrupo"),
+      saldoPrevisto: document.getElementById("saldoPrevistoGrupo"),
+    },
+    1: {
+      receberPago: document.getElementById("receberPagoFilial1"),
+      receberTitulo: document.getElementById("receberTituloFilial1"),
+      pagarPago: document.getElementById("pagarPagoFilial1"),
+      pagarTitulo: document.getElementById("pagarTituloFilial1"),
+      saldoDisponivel: document.getElementById("saldoDisponivelFilial1"),
+      saldoPrevisto: document.getElementById("saldoPrevistoFilial1"),
+    },
+    2: {
+      receberPago: document.getElementById("receberPagoFilial2"),
+      receberTitulo: document.getElementById("receberTituloFilial2"),
+      pagarPago: document.getElementById("pagarPagoFilial2"),
+      pagarTitulo: document.getElementById("pagarTituloFilial2"),
+      saldoDisponivel: document.getElementById("saldoDisponivelFilial2"),
+      saldoPrevisto: document.getElementById("saldoPrevistoFilial2"),
+    },
+    3: {
+      receberPago: document.getElementById("receberPagoFilial3"),
+      receberTitulo: document.getElementById("receberTituloFilial3"),
+      pagarPago: document.getElementById("pagarPagoFilial3"),
+      pagarTitulo: document.getElementById("pagarTituloFilial3"),
+      saldoDisponivel: document.getElementById("saldoDisponivelFilial3"),
+      saldoPrevisto: document.getElementById("saldoPrevistoFilial3"),
+    },
+  };
+let updateTimer; // Timer para atualização automática
 
-  // Formata um número para o formato de moeda brasileira
+  // Funções utilitárias
+  // Função para formatar número em formato de moeda
   function formatarMoeda(numero) {
     return numero.toLocaleString("pt-BR", {
       style: "currency",
@@ -49,7 +49,32 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  // Busca dados da API e trata erros de rede ou resposta
+  // Função para validar datas
+  function validarDatas(dataInicial, dataFinal) {
+    return new Date(dataInicial) <= new Date(dataFinal);
+  }
+
+  // Função para formatar datas
+  function formatarData(data) {
+    return data.split("-").reverse().join("-");
+  }
+
+  // Função definir as cores dos valores dos KPIs
+  function definirCorDoValor(valorFormatado, elemento) {
+    // Removendo o símbolo de moeda e convertendo para número
+    const valor = Number(valorFormatado.replace(/R\$\s?|(\.)/g, '').replace(',', '.'));
+  
+    // Definindo a cor baseada no valor
+    if (valor > 0) {
+      elemento.style.color = 'green';
+    } else if (valor < 0) {
+      elemento.style.color = 'red';
+    } else {
+      elemento.style.color = 'black';
+    }
+  }
+  // Comunicação com a API
+  // Função para buscar dados da API
   async function buscarDadosAPI(apiUrl) {
     try {
       const response = await fetch(apiUrl);
@@ -64,8 +89,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  
-  
+  // Função para buscar dados para o gráfico
   async function buscarDadosFluxoDeCaixaDia() {
     const apiUrl = `https://192.168.121.145:4445/fluxo_de_caixa_dia?data_inicial=${formatarData(dataInicialInput.value)}&data_final=${formatarData(dataFinalInput.value)}`;
     try {
@@ -76,31 +100,15 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-
-  function definirCorDoValor(valorFormatado, elemento) {
-    // Removendo o símbolo de moeda e convertendo para número
-    const valor = Number(valorFormatado.replace(/R\$\s?|(\.)/g, '').replace(',', '.'));
-  
-    // Definindo a cor baseada no valor
-    if (valor > 0) {
-      elemento.style.color = 'green';
-    } else if (valor < 0) {
-      elemento.style.color = 'red';
-    } else {
-      elemento.style.color = 'black';
-    }
-  }
-  
-
-
-  // Atualiza os KPIs de Contas a Receber/Pagar
+  // Funções de atualização e processamento de dados
+  // Atualiza KPIs de Contas a Receber/Pagar
   async function atualizarKPIsContas() {
     const apiUrl = `https://192.168.121.145:4445/fluxo_de_caixa?data_inicial=${formatarData(dataInicialInput.value)}&data_final=${formatarData(dataFinalInput.value)}`;
     const dados = await buscarDadosAPI(apiUrl);
     processarKPIsContas(dados);
   }
 
-  // Processa e exibe os KPIs de Contas a Receber/Pagar
+  // Processa KPIs de Contas a Receber/Pagar
   function processarKPIsContas(dados) {
     let totalReceberPagoGrupo = 0;
     let totalReceberTituloGrupo = 0;
@@ -162,15 +170,13 @@ document.addEventListener("DOMContentLoaded", function () {
     definirCorDoValor(kpis.grupoBRF1.saldoPrevisto.textContent, kpis.grupoBRF1.saldoPrevisto);
   }
 
-
-
-  // Atualiza os saldos dos bancos
+  // Atualiza saldos dos bancos
   async function atualizarSaldosBanco() {
     const dadosSaldoBanco = await buscarDadosAPI("https://sga.grupobrf1.com:4445/saldo_banco");
     processarSaldosBanco(dadosSaldoBanco);
   }
 
-  // Processa e exibe os saldos dos bancos
+  // Processa saldos dos bancos
   function processarSaldosBanco(dados) {
     let saldoTotalGrupoBRF1 = 0;
     const saldosPorFilial = {
@@ -213,31 +219,67 @@ document.addEventListener("DOMContentLoaded", function () {
     containerGrupoBRF1.appendChild(totalElemento);
   }
 
+  // Gestão de gráficos
+  let graficoFluxoDeCaixa = null; // Variável global para armazenar o gráfico
+
+  // Cria gráfico de fluxo de caixa
+  function criarGraficoFluxoDeCaixa(dados) {
+    const labels = dados.map(d => d.DTVENC);
+    const saldoDisponivelData = dados.map(d => d.SALDO_DISP);
+    const saldoPrevistoData = dados.map(d => d.SALDO_PREVISTO);
+
+    if (graficoFluxoDeCaixa) {
+        // Gráfico já existe, atualize os dados
+        graficoFluxoDeCaixa.data.labels = labels;
+        graficoFluxoDeCaixa.data.datasets[0].data = saldoDisponivelData;
+        graficoFluxoDeCaixa.data.datasets[1].data = saldoPrevistoData;
+        graficoFluxoDeCaixa.update();
+    } else {
+        // Gráfico não existe, crie um novo
+        const ctx = document.getElementById('graficoFluxoDeCaixa').getContext('2d');
+        graficoFluxoDeCaixa = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Saldo Disponível',
+                    data: saldoDisponivelData,
+                    borderColor: 'rgb(54, 162, 235)',
+                    tension: 0.1
+                }, {
+                    label: 'Saldo Previsto',
+                    data: saldoPrevistoData,
+                    borderColor: 'rgb(255, 99, 132)',
+                    tension: 0.1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+}
+
+  // Temporizador e eventos da interface
   // Inicia o temporizador para atualização automática
   function iniciarTemporizador() {
-      pararTemporizador();
-      updateTimer = setInterval(atualizarTodosKPIs, 2000);
+    pararTemporizador();
+    updateTimer = setInterval(atualizarTodosKPIs, 2000);
   }
 
   // Para o temporizador de atualização automática
   function pararTemporizador() {
-      if (updateTimer) {
-        clearInterval(updateTimer);
-        updateTimer = null; // Adicione esta linha para limpar a referência
-      }
+    if (updateTimer) {
+      clearInterval(updateTimer);
+      updateTimer = null; // Adicione esta linha para limpar a referência
     }
-
-  // Valida se a data inicial é menor ou igual à data final
-  function validarDatas(dataInicial, dataFinal) {
-    return new Date(dataInicial) <= new Date(dataFinal);
   }
 
-  // Formata a data para o formato "DD-MM-AAAA"
-  function formatarData(data) {
-    return data.split("-").reverse().join("-");
-  }
-
-  // Atualiza todos os KPIs (Contas e Saldos)
+  // Atualiza todos os KPIs (Contas, Saldos e Gráfico)
   async function atualizarTodosKPIs() {
     if (!validarDatas(dataInicialInput.value, dataFinalInput.value)) {
         alert("Erro: A data inicial não pode ser maior que a data final.");
@@ -248,64 +290,28 @@ document.addEventListener("DOMContentLoaded", function () {
     await buscarDadosFluxoDeCaixaDia(); // Atualiza o gráfico
   }
 
-  pesquisarButton.addEventListener("click", atualizarTodosKPIs);
-  buscarAcumuladoMesAtual();
+  // Evento de clique para o botão de pesquisa
+  pesquisarButton.addEventListener("click", function() {
+    pararTemporizador(); // Parar a atualização automática ao realizar uma pesquisa manual
+    atualizarTodosKPIs(); // Realizar a atualização de dados manualmente
+  });
 
-  let graficoFluxoDeCaixa = null; // Variável global para armazenar o gráfico
+  // Busca o acumulado do mês atual ao carregar a página
+  async function buscarAcumuladoMesAtual() {
+    const dataAtual = new Date();
+    const ano = dataAtual.getFullYear();
+    const mes = (dataAtual.getMonth() + 1).toString().padStart(2, "0");
+    const primeiroDiaDoMes = "01";
+    const diaAtual = dataAtual.getDate().toString().padStart(2, "0"); // Dia atual
 
+    const dataInicialFormatada = formatarData(`${ano}-${mes}-${primeiroDiaDoMes}`);
+    const dataFinalFormatada = formatarData(`${ano}-${mes}-${diaAtual}`);
 
-  function criarGraficoFluxoDeCaixa(dados) {
-    // Destruir o gráfico existente, se houver
-    if (graficoFluxoDeCaixa) {
-        graficoFluxoDeCaixa.destroy();
-    }
-
-    // Processar os dados para o formato necessário pelo Chart.js
-    const labels = dados.map(d => d.DTVENC);
-    const saldoDisponivelData = dados.map(d => d.SALDO_DISP);
-    const saldoPrevistoData = dados.map(d => d.SALDO_PREVISTO);
-
-    const ctx = document.getElementById('graficoFluxoDeCaixa').getContext('2d');
-    graficoFluxoDeCaixa = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Saldo Disponível',
-                data: saldoDisponivelData,
-                borderColor: 'rgb(54, 162, 235)',
-                tension: 0.1
-            }, {
-                label: 'Saldo Previsto',
-                data: saldoPrevistoData,
-                borderColor: 'rgb(255, 99, 132)',
-                tension: 0.1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
+    dataInicialInput.value = `${ano}-${mes}-${primeiroDiaDoMes}`;
+    dataFinalInput.value = `${ano}-${mes}-${diaAtual}`; // Define a data final como hoje
+    await atualizarTodosKPIs();
+    iniciarTemporizador();
   }
 
-    // Busca o acumulado do mês atual até a data atual ao carregar a página
-    async function buscarAcumuladoMesAtual() {
-      const dataAtual = new Date();
-      const ano = dataAtual.getFullYear();
-      const mes = (dataAtual.getMonth() + 1).toString().padStart(2, "0");
-      const primeiroDiaDoMes = "01";
-      const diaAtual = dataAtual.getDate().toString().padStart(2, "0"); // Dia atual
-
-      const dataInicialFormatada = formatarData(`${ano}-${mes}-${primeiroDiaDoMes}`);
-      const dataFinalFormatada = formatarData(`${ano}-${mes}-${diaAtual}`);
-
-      dataInicialInput.value = `${ano}-${mes}-${primeiroDiaDoMes}`;
-      dataFinalInput.value = `${ano}-${mes}-${diaAtual}`; // Define a data final como hoje
-      await atualizarTodosKPIs();
-      iniciarTemporizador();
-    }
-  });
+  buscarAcumuladoMesAtual(); // Executa ao carregar a página
+});
